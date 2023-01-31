8e20da0e1053ef0564cfd0c342ce922e674153ce
Vinicius Mariano <93166937+ViniciusMariano02@users.noreply.github.com>
2023-01-31 10:21:41 -0300

    Add files via upload

---
+++ src/components/products-table/ProductsTable.js
@@ -0,0 +1,232 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import "./ProductsTable.css";
+import logo2 from "../assets/logo2.png"
+import {loginLogout} from '../../redux/loginSlice';
+import { Link , useNavigate} from "react-router-dom";
+import { useDispatch } from 'react-redux';
+
+export const Products = () => {
+    const [products, setProducts] = useState([]);
+    const [selectedQuotation, setSelectedQuotation] = useState();
+
+    useEffect(() => {
+        async function fetchData (){
+            const response = await fetch ("http://8b38091fc43d.sn.mynetname.net:2000/cotacao/fornecedor/529"); 
+            const data = await response.json();
+            setProducts(data);
+        }
+        fetchData();
+    }, []);
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    function onChangeLogin(){
+        dispatch(loginLogout())
+        navigate('/')
+    }
+
+    function getFilteredList(){
+        if (!selectedQuotation) {
+            return products;
+        }
+        return products.filter((product) => product.descricao === selectedQuotation)
+    }
+
+    var filteredList = useMemo(getFilteredList, [selectedQuotation, products]);
+
+    function handleQuotationChange(event){
+        setSelectedQuotation(event.target.value);
+    }
+
+    const [isEdit, setEdit] = useState(false);
+      
+      console.log(products);
+    
+
+    const handleEdit = () =>{
+        setEdit(!isEdit);
+    }
+
+    const detalheList = [];
+
+    products.map(object => object.values).forEach((detalhar) => {
+        detalheList.push(products[1])
+    });
+
+    console.log(detalheList)
+
+    const handleInputChange01 = (e, index) =>{
+        const {name, value} = e.target;
+        const list = [...products];
+        list[index][name] = value
+        setProducts(list);
+    }
+
+    const handleInputChange = (e, index) =>{
+            const {name, value} = e.target; 
+            const list = [...products]; 
+            list[index][name] = value; 
+            setProducts(list); 
+    }
+
+    const handleSave = async(e) => {
+            e.preventDefault();
+            fetch("http://8b38091fc43d.sn.mynetname.net:2000/cotacaoDetalhe/save",{ 
+                method:"POST", 
+                headers:{"content-type":"application/json"}, 
+                body:JSON.stringify(detalheList) 
+            }).then((res)=>{
+                if(res.status === 200 || res.status === 201){ 
+                    setEdit(!isEdit); 
+                    alert('Salvo com sucesso.'); 
+                }
+            }).catch((err)=>{
+                console.log(err.message)  
+            })
+    }
+
+    return(
+        <div>
+            <div>
+                <header className="geral-header">
+                    <img className="home-image" src={logo2} alt='G3'/><p className="marca">MASTERBOI LTDA <button className="exit" onClick={onChangeLogin}>Sair</button> </p>
+                </header>
+            </div>
+            <div className="product-list">
+            <div className="product">
+                <div className="card mt-5 mb-5">
+                    <div className="card-header">
+                        <Link to ="/home" className="op1">Principal</Link>
+                        <Link to ="/table" className="op2">Cotação</Link> 
+                    </div>
+                    <div className="card-body p-0">                     
+                                <div className="select" >
+                                    <select 
+                                        name="quotation-list"
+                                        id="quotation-list"
+                                        onChange={handleQuotationChange}
+                                    >  
+                                    <option value="None">Selecione a cotação</option>
+                                    {products.map((item) => {
+                                        return (
+                                            <option key={item.id} value={item.descricao}>{item.id}-{item.descricao}</option>
+                                        )
+                                    })}
+
+                                    </select>
+                                </div>
+                        <form onSubmit={handleSave} className="table-responsive"> {}
+                            <table className="table mb-0">
+                                <thead>
+                                    <tr>
+                                        <th className="border-top-01" scope="col">
+                                            Descrição
+                                        </th>
+                                        <th className="border-top-02">
+                                            Valor
+                                        </th>
+                                        <th className="border-top-03">
+                                            Embalagem
+                                        </th>
+                                        <th className="border-top-04">
+                                            Código de Barra
+                                        </th>
+                                        <th className="border-top-05">
+                                            Código
+                                        </th>
+                                        <th className="border-top-06">
+                                            Observação
+                                        </th>
+                                        <th className="border-top-07">
+                                            Quantidade
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    
+                                    {products.length ? (
+
+                                        filteredList.map((products)  => (
+                                            products.detalhe.map((detalhes, i) => {
+                                                return (
+                                                        <tr key={detalhes.id}>
+                                                        <td className="placeholder">
+                                                            {detalhes.descricao}
+                                                        </td>
+                                                        <td className= "edit-input" onDoubleClick={handleEdit}>{}
+                                                            {isEdit ? 
+                                                            (
+                                                            <input 
+                                                                className= "edit-input"
+                                                                value={detalhes.valor_custo_fornecedor }  
+                                                                name="valor_custo_fornecedor"
+                                                                onChange={(e) => handleInputChange(e, i)} 
+                                                            />
+                                                            ) : (detalhes.valor_custo_fornecedor )} {}
+                                                        </td>
+                                                        <td id='category' className= "placeholder02" >
+                                                            {detalhes.unidade}
+                                                        </td>
+                                                        <td className= "placeholder03">
+                                                            {detalhes.gtin}
+                                                        </td>
+                                                        <td className= "placeholder04">
+                                                            {detalhes.id_produto}
+                                                        </td>
+                                                        <td className= "placeholder05" onDoubleClick={handleEdit}>{isEdit ? (
+                                                            <input className= "placeholder05" value={detalhes.observacao}
+                                                            name="observacao"
+                                                            onChange={(e) => handleInputChange01(e, i)}
+                                                            />
+                                                                
+                                                        ) : (detalhes.observacao)}
+                                                        </td>
+
+                                                        <td className= "placeholder06">
+                                                            {detalhes.quantidade}
+                                                        </td>
+                                                    </tr>
+                                                    
+                                                )
+
+
+                                            })                                
+                                                
+                                        ))
+                                            
+                                        ) : (
+                                            <tr>
+                                                <td colspan="4" className="text-danger"> A lista está vazia! </td>
+                                            </tr>
+                                        )
+                                    }
+                                    
+
+                                </tbody>
+                            </table>
+                            <div>
+                            <button className="edit" onClick={handleEdit}>
+                                <i> 
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    </svg>
+                                </i>
+                            </button>
+                            <button className="save" onSubmit={handleSave}> Salvar </button> {}
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+    );
+}
+
+
+
+export default Products
