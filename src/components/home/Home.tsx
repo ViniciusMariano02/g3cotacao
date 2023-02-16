c0d5f3b3ee05fccece36cc08a13f61e7a55a2e9a
Vinicius Mariano <93166937+ViniciusMariano02@users.noreply.github.com>
2023-02-16 14:11:35 -0300

    Add files via upload

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,48 @@
+import logo2 from "./logo2.png"
+import './Home.css'
+import {loginLogout} from '../../redux/loginSlice';
+import { Link , useNavigate} from "react-router-dom";
+import { useDispatch } from 'react-redux';
+
+
+function Home(){
+    
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    function onChangeLogin(){
+        dispatch(loginLogout())
+        navigate('/')
+    }
+
+    return(
+        <div className='geral'>
+            <header className="app-header3">
+                <div className='content'>
+
+                    <span><img src={logo2} className="imageHome" alt='G3'/></span>
+
+                    <h1 className="app-title2"> Cotação </h1>
+
+                    <p className='Brand'>  <button onClick={onChangeLogin} className='link'>Sair</button> </p>
+
+                </div>
+            </header>
+            
+
+            <div className="btns">
+                <Link to ="/home" className="op1">Principal</Link>
+                <Link to ="/table" className="op2">Cotação</Link>
+            </div>
+
+            <div><p className="texto"> <i><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon"><path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" /></svg></i> Atenção Fornecedor</p></div>
+                
+                
+            <footer className="Ver">Versão 1.23.0, G3 SOLUÇÕES EM TECNOLOGIA.</footer>
+            
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/login/Login.js
@@ -0,0 +1,136 @@
+import React, { useState  } from 'react';
+import './Login.css';
+import TextField from '@mui/material/TextField';
+import {Button} from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { store } from '../../redux/store';
+import { loginEnter, loginError } from '../../redux/loginSlice';
+import { useNavigate } from 'react-router-dom';
+//import { verifyCredentials } from '../../services/authService';
+import logo2 from './logo2.png';
+import Home from '../home/Home.tsx'
+
+
+function Login() {
+
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [user, setUser] = useState([]);
+  
+  const navigate = useNavigate();
+
+  function funçoes (){
+    users();
+    login();
+  }
+
+  async function users() 
+  {
+    const result = await fetch (`http://8b38091fc43d.sn.mynetname.net:2000/user/${userId}/${password} `, {
+    })
+
+    const data = await result.json();          
+    setUser(data);
+  }
+  
+  
+  async function login() 
+    {
+      const result = await fetch (`http://8b38091fc43d.sn.mynetname.net:2000/user/${userId}/${password} `, {
+      })
+      .then(async (res) => {
+        if( res.status === 200){
+          dispatch(loginEnter());
+        }
+        else{
+          dispatch(loginError());
+        }
+      }).catch((err)=>{
+        console.log(err.message)      
+    }, []);
+    }
+
+  console.log(user)
+
+    
+  store.subscribe(() => {
+    setErrorMessage(store.getState().login.errorMessage);
+      if(store.getState().login.isLogged === true)
+        navigate('/home');
+        })
+
+  const dispatch = useDispatch()
+
+  function onChangeUserName(e){
+    setUserId(e.currentTarget.value)
+  }
+  
+  function onChangePassword(e){
+    setPassword(e.currentTarget.value)
+  }
+
+  document.addEventListener('keydown', function(e) {
+    if(e.key === "Enter"){
+      document.getElementById("btEntrar").click();
+    }
+  });
+
+  <Home user={user} />
+
+  //function onClickEnter(){
+    //if( verifyCredentials(userId, password) )
+      //dispatch(loginEnter());
+    //else
+      //dispatch(loginError());
+  //}
+  
+
+
+
+  return (
+  
+      <header className="login-header">
+        <div className="box">  
+
+          <div className="login-message">
+            <span className= "login-form-tittle"><img className="image" src={logo2} alt ="G3"/></span>
+          </div>
+
+          <TextField
+              className='wrap-input'
+              required
+              id="user-name"
+              label="Código"
+              defaultValue=""
+              onChange={onChangeUserName}
+            />
+
+          <TextField
+              className='wrap-input'
+              required
+              id="user-password"
+              label="Senha"
+              defaultValue=""
+              type="password"
+              onChange={onChangePassword}
+            />
+
+          <div className="login-error">
+            {errorMessage}
+          </div>
+
+            <Button className='btn' id='btEntrar' variant="contained" onClick={funçoes} >Login</Button>
+
+
+        </div>
+      </header>  
+      
+  );
+}
+
+
+
+export default Login;
+
+
