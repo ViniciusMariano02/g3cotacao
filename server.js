aa28f2a54121f2e05c6512ff739ce0b38810b580
Vinicius Mariano <93166937+ViniciusMariano02@users.noreply.github.com>
2023-02-28 08:54:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 
 const app =express()
 
@@ -13,6 +14,13 @@ app.use('/',
         )       
     )
 )
+
+ if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+      });
+}
 
 app.listen(process.env.PORT || 3000, (err) => { 
     if (err) {return console.log(err)}
